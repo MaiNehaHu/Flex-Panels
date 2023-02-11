8e366d2d1ad48d3a4f9c5cce79e787c920e27746
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2023-02-12 04:54:24 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+let image = document.querySelectorAll(".panel");
+
+/**for image flex on click */
+function Open() {
+ this.classList.toggle('open'); /**adds and removes */
+}
+/**for text transition on click */
+function toggleActive(e){
+    console.log(e.propertyName);
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active')
+    }
+
+}
+image.forEach((panel) =>panel.addEventListener('click',Open));
+image.forEach((panel) => panel.addEventListener('transitionend',toggleActive));
