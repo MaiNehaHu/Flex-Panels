1229436e8c1ad63f9cca16ef41c97fe86a8c7da3
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2023-07-28 12:06:43 +0530

    responsive

--- Script.js
+++ Script.js
@@ -2,15 +2,14 @@ let image = document.querySelectorAll(".panel");
 
 /**for image flex on click */
 function Open() {
- this.classList.toggle('open'); /**adds and removes */
+  this.classList.toggle("open"); /**adds and removes */
 }
 /**for text transition on click */
-function toggleActive(e){
-    console.log(e.propertyName);
-    if(e.propertyName.includes('flex')){
-        this.classList.toggle('open-active')
-    }
-
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
 }
-image.forEach((panel) =>panel.addEventListener('click',Open));
-image.forEach((panel) => panel.addEventListener('transitionend',toggleActive));
+image.forEach((panel) => panel.addEventListener("click", Open));
+image.forEach((panel) => panel.addEventListener("transitionend", toggleActive));
